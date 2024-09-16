d045112aa233269fb63ce76480ff7eb4355013c1
jaaeger <iwan.egerev@yandex.ru>
2024-09-16 15:33:20 +0300

    16.09.2024

---
+++ neetcode/1.4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = {}
+        for word in strs:
+            counts = [0] * 26
+            for char in word:
+                counts[ord(char)-ord("a")]+=1
+            key = tuple(counts)
+            if key not in result:
+                result[key]=[]
+            result[key].append(word)
+        return result.values()
